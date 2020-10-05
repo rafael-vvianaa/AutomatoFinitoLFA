ab6d00203c662b006474885707de4384db9ab3fa
Rafael <rafael-vvianaa@hotmail.com>
2020-10-05 19:32:32 -0300

    Finalizando o automato

--- src/Automato/Automato_ini.java
+++ src/Automato/Automato_ini.java
@@ -19,4 +19,40 @@ public class Automato_ini{
 		this.auto_Tabela = _autotabela;
    }
 	
+	public String existeTransicao( String auto_estadoAtual, String simbolo_da_Fita){
+		String transicao = "x";
+		for(int linha = 0; linha <= auto_Estados.size(); linha++ ){
+			for(int coluna = 0; coluna <= auto_Alfabeto.size(); coluna++){
+				if(auto_Estados.get(linha).equals(auto_estadoAtual) && auto_Alfabeto.get(coluna).equals(simbolo_da_Fita)){
+					if(auto_Tabela[linha][coluna] != "x"){
+						transicao = auto_Tabela[linha][coluna];
+						return transicao;
+					}
+				}
+			}
+		}
+		return transicao;
+	}
+	
+		public boolean validacao_palavra(String[] fita){
+			String auto_estadoAtual = this.getAuto_EstadoInicial();
+			String transicao = "";
+			for(int i = 0; i < fita.length; i++){
+				transicao = existeTransicao( auto_estadoAtual, fita[i]);
+				if(transicao != "x"){
+					auto_estadoAtual = transicao;
+				}else{
+					return false;
+				}
+			}
+			
+			if(auto_EstadosFinais.contains(auto_estadoAtual)){  
+				return true;
+			}else{
+				return false;
+			}
+			
+		}
+		
+		
 }
